4d5cdd46e89c6db231a0b5e18f97df799c1b8eed
unknown <daniolmedo855@gmail.com>
2025-01-21 14:25:55 +0100

    especies

--- js/animales.js
+++ js/animales.js
@@ -8,7 +8,8 @@ var felinos = [
         "color": "Dorado",
         "sexo": "Macho",
         "edad": "8 años",
-        "historia": "En el documental el león es presentado como el majestuoso rey de la sabana africana. Este ejemplar, llamado 'Mufasa' por los investigadores, lidera una manada de más de 10 miembros. Su imponente rugido puede escucharse a kilómetros de distancia, y se utiliza para marcar su territorio y coordinarse con la manada. El equipo de filmación capturó momentos íntimos de su vida, mostrando cómo enseña a sus cachorros las habilidades necesarias para sobrevivir en un entorno lleno de peligros."
+        "historia": "En el documental el león es presentado como el majestuoso rey de la sabana africana. Este ejemplar, llamado 'Mufasa' por los investigadores, lidera una manada de más de 10 miembros. Su imponente rugido puede escucharse a kilómetros de distancia, y se utiliza para marcar su territorio y coordinarse con la manada. El equipo de filmación capturó momentos íntimos de su vida, mostrando cómo enseña a sus cachorros las habilidades necesarias para sobrevivir en un entorno lleno de peligros.",
+        "habitat": "../img/habitat/leon.jpg"
     },
     {
         "id": 2,
@@ -19,7 +20,8 @@ var felinos = [
         "color": "Naranja con rayas negras",
         "sexo": "Hembra",
         "edad": "5 años",
-        "historia": "Durante la grabación los tigres de Bengala sorprendieron al equipo por su sigilo y poder. Esta hembra, llamada 'Shakti', fue seguida en las densas selvas de la India. Se documentaron sus habilidades como cazadora, especialmente su capacidad para acechar presas a distancias cortas y atacar con precisión. Las cámaras también revelaron su lado maternal, mostrando cómo cuidaba de sus dos cachorros mientras enfrentaba amenazas de otros depredadores y la constante presión de la actividad humana en su hábitat."
+        "historia": "Durante la grabación los tigres de Bengala sorprendieron al equipo por su sigilo y poder. Esta hembra, llamada 'Shakti', fue seguida en las densas selvas de la India. Se documentaron sus habilidades como cazadora, especialmente su capacidad para acechar presas a distancias cortas y atacar con precisión. Las cámaras también revelaron su lado maternal, mostrando cómo cuidaba de sus dos cachorros mientras enfrentaba amenazas de otros depredadores y la constante presión de la actividad humana en su hábitat.",
+        "habitat": "../img/habitat/tigre.jpg"
     },
     {
         "id": 3,
@@ -30,18 +32,8 @@ var felinos = [
         "color": "Amarillo con manchas negras",
         "sexo": "Macho",
         "edad": "6 años",
-        "historia": "El leopardo es uno de los protagonistas más enigmáticos de Grandes Felinos. Apodado 'Sombras' por los cineastas, este macho africano destacó por su capacidad para moverse sin ser detectado, incluso en paisajes abiertos. Su historia incluye impresionantes tomas de cómo arrastra presas más grandes que él hasta las ramas de un árbol, demostrando una fuerza extraordinaria. Este comportamiento lo protege de hienas y leones que podrían robarle su comida. El documental también resalta cómo la fragmentación de su hábitat amenaza su futuro."
-    },
-    {
-        "id": 4,
-        "imagen": "../img/felinos/jaguar.jpg",
-        "nombre": "Jaguar",
-        "especie": "Panthera onca",
-        "raza": "Americano",
-        "color": "Amarillo con rosetas negras",
-        "sexo": "Hembra",
-        "edad": "7 años",
-        "historia": "En las profundidades de la selva amazónica, el equipo siguió a 'Xul', una jaguar hembra conocida por su habilidad para cazar en el agua. Las cámaras capturaron imágenes únicas de Xul mientras acechaba a un caimán, demostrando la potencia de su mordida, capaz de atravesar huesos y caparazones. Su historia destaca los retos que enfrenta, desde la pérdida de hábitat hasta la caza furtiva. El documental busca inspirar a la audiencia a proteger a esta especie icónica de América Latina."
+        "historia": "El leopardo es uno de los protagonistas más enigmáticos de Grandes Felinos. Apodado 'Sombras' por los cineastas, este macho africano destacó por su capacidad para moverse sin ser detectado, incluso en paisajes abiertos. Su historia incluye impresionantes tomas de cómo arrastra presas más grandes que él hasta las ramas de un árbol, demostrando una fuerza extraordinaria. Este comportamiento lo protege de hienas y leones que podrían robarle su comida. El documental también resalta cómo la fragmentación de su hábitat amenaza su futuro.",
+        "habitat": "../img/habitat/leopardo.jpg"
     },
     {
         "id": 5,
@@ -52,6 +44,19 @@ var felinos = [
         "color": "Amarillo con manchas negras",
         "sexo": "Macho",
         "edad": "4 años",
-        "historia": "En las llanuras abiertas de África, 'Rayo' un joven guepardo que aprende a sobrevivir en un entorno lleno de desafíos. Las cámaras capturaron momentos emocionantes de Rayo alcanzando velocidades increíbles para cazar gacelas, mostrando por qué es el animal terrestre más rápido del mundo. A pesar de su velocidad, el guepardo enfrenta constantes amenazas de otros depredadores como leones y hienas, quienes roban sus presas. Este segmento del documental destaca la importancia de preservar los espacios abiertos para que estos velocistas puedan prosperar."
+        "historia": "En las llanuras abiertas de África, 'Rayo' un joven guepardo que aprende a sobrevivir en un entorno lleno de desafíos. Las cámaras capturaron momentos emocionantes de Rayo alcanzando velocidades increíbles para cazar gacelas, mostrando por qué es el animal terrestre más rápido del mundo. A pesar de su velocidad, el guepardo enfrenta constantes amenazas de otros depredadores como leones y hienas, quienes roban sus presas. Este segmento del documental destaca la importancia de preservar los espacios abiertos para que estos velocistas puedan prosperar.",
+        "habitat": "../img/habitat/guepardo.jpg"
+    },
+    {
+        "id": 4,
+        "imagen": "../img/felinos/pantera.jpg",
+        "nombre": "Pantera",
+        "especie": "Panthera pardus",
+        "raza": "Africano",
+        "color": "Amarillo con manchas negras",
+        "sexo": "Macho",
+        "edad": "5 años",
+        "historia": "La pantera es una de las especies más misteriosas del continente africano. En el documental, un ejemplar conocido como 'Sombra' se sigue a través de la espesa selva donde es capaz de moverse sigilosamente entre los árboles. Su comportamiento furtivo le permite cazar sin ser detectado. A pesar de su fuerza, también enfrenta los peligros de la destrucción de su hábitat y la caza furtiva. La historia de 'Sombra' refleja la difícil lucha por la supervivencia de las panteras en la naturaleza.",
+        "habitat": "../img/habitat/pantera.jpg"
     }
 ];
